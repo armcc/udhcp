979882a6cb466fdc9557ad18ea5ec977d872fac0
Russ Dill <Russ.Dill@asu.edu>
2001-08-11 00:56:37 +0000

    added requested option list, comments, and a newline

--- dhcpc.h
+++ dhcpc.h
@@ -13,15 +13,23 @@
 #define RELEASED	7
 
 
+/* Paramaters the client should request from the server */
+#define PARM_REQUESTS \
+	DHCP_SUBNET, \
+	DHCP_ROUTER, \
+	DHCP_DNS_SERVER, \
+	DHCP_HOST_NAME, \
+	DHCP_DOMAIN_NAME, \
+	DHCP_BROADCAST
 
 
 struct client_config_t {
-	char *dir;
-	char *prefix;
-	char interface[10];
-	char *clientid;
-	int ifindex;
-	unsigned char arp[6];
+	char *dir;			/* Path containing DHCP client scripts */
+	char *prefix;			/* Prefix to add to scripts */
+	char interface[10];		/* The name of the interface to use */
+	char *clientid;			/* Optional client id to use */
+	int ifindex;			/* Index number of the interface to use */
+	unsigned char arp[6];		/* Our arp address */
 };
 
 extern struct client_config_t client_config;
