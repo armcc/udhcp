93c486865134768aa06b333902904f0cd1f6a854
Russ Dill <Russ.Dill@asu.edu>
2002-03-23 00:34:03 +0000

    little commenting in options.h

--- options.h
+++ options.h
@@ -18,8 +18,8 @@ enum {
 	OPTION_S32
 };
 
-#define OPTION_REQ	0x10
-#define OPTION_LIST	0x20
+#define OPTION_REQ	0x10 /* have the client request this option */
+#define OPTION_LIST	0x20 /* There can be a list of 1 or more of these */
 
 struct dhcp_option {
 	char name[10];
