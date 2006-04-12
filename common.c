3da65ce484c24357aceae8b906623e0b540fdece
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-04-12 18:17:00 +0000

    - add remark about bb_xdaemon. Need to put a copy into libbb_udhcp.h for the   standalone version.

--- common.c
+++ common.c
@@ -1,3 +1,4 @@
+/* vi: set sw=4 ts=4: */
 /* common.c
  *
  * Functions for debugging and logging as well as some other
@@ -6,19 +7,7 @@
  * Russ Dill <Russ.Dill@asu.edu> 2001-2003
  * Rewritten by Vladimir Oleynik <dzo@simtreas.ru> (C) 2003
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
  */
 
 #include <fcntl.h>
@@ -68,7 +57,7 @@ void background(const char *pidfile)
 
 	/* hold lock during fork. */
 	pid_fd = pidfile_acquire(pidfile);
-	if (daemon(0, 0) == -1) {
+	if (daemon(0, 0) == -1) { /* bb_xdaemon? */
 		perror("fork");
 		exit(1);
 	}
